43b6299daee5651b20e40e6015fd0021f515b790
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2023-09-17 17:02:53 +0530

    Finalized

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "styles/Footer.module.scss";
+import GetIcon from "components/GetIcon";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+    </footer>
+  );
+};
+
+export default Footer;
